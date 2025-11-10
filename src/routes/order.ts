2a78bf23d023de7131ecb012fa2b717941965a6e
uwishema11 <uwishemaceline4@gmail.com>
2025-11-11 01:05:42 +0200

    users shoule be able to get their order history

--- src/routes/order.ts
+++ src/routes/order.ts
@@ -6,5 +6,5 @@ import protectedRoute from '../middleware/verifyAuth';
 const orderRouter = express.Router();
 
 orderRouter.post('/create', protectedRoute, orderController.placeOrder);
-
+orderRouter.get('/', protectedRoute, orderController.getOrdersController);
 export default orderRouter;
